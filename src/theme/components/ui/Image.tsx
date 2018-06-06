090b85dcb32c19f5578836f9be2c8733e05c561c
Pedro Nauck <pedronauck@gmail.com>
2018-06-06 03:00:17 -0300

    feat: add lightbox to iamge

--- src/theme/components/ui/Image.tsx
+++ src/theme/components/ui/Image.tsx
@@ -1,8 +1,73 @@
+import * as React from 'react'
+import { SFC } from 'react'
+import { adopt } from 'react-adopt'
+import { Toggle, Hover } from 'react-powerplug'
+import { ZoomIn } from 'react-feather'
+import Lightbox from 'react-images'
 import styled from 'react-emotion'
 
-export const Image = styled('img')`
-  padding: 5px;
+const Wrapper = styled('div')`
+  position: relative;
   margin: 30px 0;
-  width: 100%;
   border: 1px solid ${p => p.theme.colors.grayLight};
 `
+
+const ImageStyled = styled('img')`
+  display: block;
+  width: 100%;
+  padding: 5px;
+`
+
+const ImageHover = styled('div')`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.4);
+`
+
+const ZoomIcon = styled(ZoomIn)`
+  stroke: white;
+
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+type ImageProps = React.ImgHTMLAttributes<any>
+
+const mapper = {
+  toggle: <Toggle initial={false} />,
+  hover: <Hover />,
+}
+
+const Composed = adopt(mapper, ({ toggle, hover }) => ({
+  ...toggle,
+  ...hover,
+}))
+
+export const Image: SFC<ImageProps> = props => (
+  <Composed>
+    {({ on, toggle, isHovered, bind }: any) => (
+      <Wrapper {...bind}>
+        {isHovered && (
+          <ImageHover>
+            <ZoomIcon size={80} onClick={toggle} />
+          </ImageHover>
+        )}
+        <ImageStyled {...props} />
+        {on && (
+          <Lightbox
+            images={[{ src: props.src }]}
+            isOpen={on}
+            onClose={toggle}
+          />
+        )}
+      </Wrapper>
+    )}
+  </Composed>
+)
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,7 @@
 declare module 'react-feather'
 declare module 'react-helmet'
+declare module 'react-powerplug'
+declare module 'react-images'
 
 declare module '*.woff2' {
   const content: any
