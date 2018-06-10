dc4543aa28ea44f903f5a2cfc59bd222d0d72790
Pedro Nauck <pedronauck@gmail.com>
2018-06-10 03:05:12 -0300

    chore: change Box component border

--- src/theme/components/ui/Box.tsx
+++ src/theme/components/ui/Box.tsx
@@ -4,6 +4,6 @@ export const Box = styled('div')`
   display: flex;
   flex-direction: column;
   background: white;
-  border: 2px solid ${p => p.theme.colors.gray};
+  border: 1px solid ${p => p.theme.colors.gray};
   border-radius: 5px;
 `
