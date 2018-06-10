e503e5761f08b6f894888418300b399ed97acc30
Pedro Nauck <pedronauck@gmail.com>
2018-06-10 05:03:28 -0300

    feat: add custom index.html with ga script

--- doczrc.js
+++ doczrc.js
@@ -5,27 +5,30 @@ import externalLinks from 'remark-external-links'
 const PUBLIC = path.resolve(__dirname, 'public')
 const SRC = path.resolve(__dirname, 'src')
 
+const modifyBundlerConfig = config => {
+  config.resolve.alias = Object.assign({}, config.resolve.alias, {
+    '@fonts': `${PUBLIC}/fonts`,
+    '@images': `${PUBLIC}/images`,
+    '@components': `${SRC}/theme/components`,
+    '@styles': `${SRC}/theme/styles`,
+  })
+
+  config.plugins.push(
+    new FaviconsWebpackPlugin({
+      logo: `${PUBLIC}/images/favicon.png`,
+      inject: true,
+    })
+  )
+
+  return config
+},
+
 export default {
   title: 'Docz',
   description: 'It has never been so easy to document your things',
-  theme: 'theme/index',
+  indexHtml: 'src/index.html',
+  theme: 'src/theme/index',
   propsParser: false,
   mdPlugins: [externalLinks.default],
-  modifyBundlerConfig: config => {
-    config.resolve.alias = Object.assign({}, config.resolve.alias, {
-      '@fonts': `${PUBLIC}/fonts`,
-      '@images': `${PUBLIC}/images`,
-      '@components': `${SRC}/theme/components`,
-      '@styles': `${SRC}/theme/styles`,
-    })
-
-    config.plugins.push(
-      new FaviconsWebpackPlugin({
-        logo: `${PUBLIC}/images/favicon.png`,
-        inject: true,
-      })
-    )
-
-    return config
-  },
+  modifyBundlerConfig
 }
