7d5b7df46e0ba9eec72d6f80a1806731e7ff9e6b
Pedro Nauck <pedronauck@gmail.com>
2018-06-11 19:23:02 -0300

    feat: add temporary sections on home

---
+++ src/docs/home/components/Sections.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react'
+import styled from 'react-emotion'
+import { withRouter } from 'react-router'
+
+import { Button } from '@components/ui'
+import image from '@images/builtin-components.png'
+
+const Wrapper = styled('div')`
+  padding: 100px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background: ${p => p.theme.colors.grayLight};
+  border-top: 1px solid ${p => p.theme.colors.gray};
+`
+
+const Title = styled('h2')`
+  font-size: 48px;
+  font-family: 'Zilla Slab';
+  margin: 0 0 40px 0;
+`
+
+const Image = styled('img')`
+  width: 800px;
+`
+
+const Text = styled('p')`
+  width: 600px;
+  margin: 40px 0;
+  text-align: center;
+`
+
+export const Sections = withRouter(({ history }) => (
+  <Wrapper>
+    <Title>Built-in Components</Title>
+    <Image src={image} alt="Built-in components" />
+    <Text>
+      Docz has some built-ins components that help you to document your things.
+      Using the power of components and making some AST parsing algorithms to
+      source data for that components, we can do a lot of things, like render on
+      the fly your components, create tables with contents, custom getters by
+      traversing your file, etc. The sky is limit here!
+    </Text>
+    <Button
+      scale="big"
+      kind="secondary"
+      onClick={() => history.push('/introduction/documenting-your-things')}
+    >
+      More information
+    </Button>
+  </Wrapper>
+))
