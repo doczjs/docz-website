37f66f517da43c1271e4d2732fd1cb1722477b6e
Kraig Walker <giarknot@hotmail.co.uk>
2018-06-21 11:40:06 +0100

    Improve some of the copy in the home page to improve readability

--- docs/home/components/Sections.tsx
+++ docs/home/components/Sections.tsx
@@ -36,9 +36,9 @@ export const Sections = withRouter(({ history }) => (
     <Title>Built-ins Components</Title>
     <Image src={image} alt="Built-in components" />
     <Text>
-      Docz has some built-ins components that help you to document your things.
-      Using the power of components and making some AST parsing algorithms to
-      source data for that components, we can do a lot of things, like render
+      Docz has some built-in components that help you to document your things.
+      Using the power of components and some AST parsing algorithms to
+      source data for that components, we can render
       your components on the fly, create tables with contents, custom getters by
       traversing your file, etc. The sky is limit here!
     </Text>
