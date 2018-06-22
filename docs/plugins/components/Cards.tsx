faf3b5a0217790f8d1934c96f249ee35a68b6129
Pedro Nauck <pedronauck@gmail.com>
2018-06-22 03:27:54 -0300

    chore: add new css plugin

--- docs/plugins/components/Cards.tsx
+++ docs/plugins/components/Cards.tsx
@@ -12,8 +12,13 @@ export const Cards: SFC = () => (
   <Wrapper>
     <Card
       name="docz-plugin-babel6"
-      description="Use this plugin if you want to use some older babel version"
+      description="Use this plugin to use older babel version"
       link="https://github.com/pedronauck/docz/tree/master/packages/docz-plugin-babel6"
+    />
+    <Card
+      name="docz-plugin-css"
+      description="Plugin to parse css files inside your documents"
+      link="https://github.com/pedronauck/docz/tree/master/packages/docz-plugin-css"
     />
   </Wrapper>
 )
