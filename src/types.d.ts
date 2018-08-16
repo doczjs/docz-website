1280126c603ab07da9ee53413c5c937574296f54
Marcelo Formentão <marcelo@precisaosistemas.com.br>
2018-08-16 13:28:45 -0300

    chore: include responsive packages and config

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,4 @@
+declare module 'react-breakpoints'
 declare module 'react-feather'
 declare module 'react-helmet'
 declare module 'react-powerplug'
@@ -20,4 +21,31 @@ declare module '*.svg' {
 declare module '*.png' {
   const content: any
   export default content
+}
+
+declare module 'facepaint' {
+  interface Styles {
+    [key: string]: string | number | Styles
+  }
+
+  interface MqStyles {
+    [key: string]: string | string[] | number | number[] | Styles
+  }
+
+  type Mq = (styles: object) => Styles
+
+  interface FacepaintSettings {
+    literal?: boolean
+    overlap?: boolean
+  }
+
+  type Facepaint = (
+    /** media queries to be applied across */
+    mediaQueries: string[],
+    settings?: FacepaintSettings
+  ) => Mq
+
+  const facepaint: Facepaint
+
+  export = facepaint
 }
