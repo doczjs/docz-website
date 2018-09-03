8654f4dbbece2a9cc7cd606b2a5431864a200220
Pedro Nauck <pedronauck@gmail.com>
2018-09-02 21:47:55 -0300

    feat: add new plugins

--- docs/plugins/components/Cards.tsx
+++ docs/plugins/components/Cards.tsx
@@ -11,14 +11,29 @@ const Wrapper = styled('div')`
 export const Cards: SFC = () => (
   <Wrapper>
     <Card
-      name="docz-plugin-babel6"
-      description="Use this plugin to use older babel version"
-      link="https://github.com/pedronauck/docz/tree/master/packages/docz-plugin-babel6"
-    />
-    <Card
       name="docz-plugin-css"
       description="Plugin to parse css files inside your documents"
       link="https://github.com/pedronauck/docz/tree/master/packages/docz-plugin-css"
+    />
+    <Card
+      name="docz-plugin-babel6"
+      description="Use this plugin to use older babel version [DEPRECATED]"
+      link="https://github.com/pedronauck/docz/tree/master/packages/docz-plugin-babel6"
+    />
+    <Card
+      name="docz-plugin-netlify"
+      description="Deploy your documentation to Netlify"
+      link="https://github.com/nicholasess/docz-plugin-netlify"
+    />
+    <Card
+      name="docz-plugin-react-native"
+      description="Allow you to use docz with React Native"
+      link="https://github.com/pedronauck/docz/blob/master/packages/docz-plugin-react-native"
+    />
+    <Card
+      name="docz-plugin-svgr"
+      description="Allow you to parse svg using svgr"
+      link="https://github.com/pedronauck/docz/blob/master/packages/docz-plugin-svgr"
     />
   </Wrapper>
 )
